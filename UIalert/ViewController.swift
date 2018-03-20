1e286b180b03dbb43376ea1651ecc3f853e40e35
gdcp <gdcp@m47.local>
2018-03-20 11:00:40 +0800

    添加alertcontroller控制器

--- UIalert/ViewController.swift
+++ UIalert/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        let alert = UIAlertController(title: "hello world", message: "请输入密码：", preferredStyle: .alert)
+        
+        let action1 = UIAlertAction(title: "确定", style: .default) { (UIAlert) in
+            print("确定")}
+        let action2 = UIAlertAction(title: "取消", style: .cancel) { (sss) in
+            print("取消")}
+        
+        alert.addAction(action1)
+        alert.addAction(action2)
+        alert.addTextField { (textfield) in
+            textfield.placeholder = "请输入密码"
+        }
+        self.present(alert, animated: true, completion: nil)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
